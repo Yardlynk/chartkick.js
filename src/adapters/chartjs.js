460697d401d84f9ed849b126ffc8a2126d6febbf
gmanninglive <gmanningdev@gmail.com>
2025-04-17 10:37:15 +0100

    feat(chartjs.js): patch addOpacity to remove setting opacity of colours to 50%

--- src/adapters/chartjs.js
+++ src/adapters/chartjs.js
@@ -108,9 +108,11 @@ function setYtitle(options, title) {
 }
 
 // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-function addOpacity(hex, opacity) {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? "rgba(" + parseInt(result[1], 16) + ", " + parseInt(result[2], 16) + ", " + parseInt(result[3], 16) + ", " + opacity + ")" : hex;
+function addOpacity(hex, _opacity) {
+  return hex;
+  // patched to remove default opacity
+  // const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  // return result ? "rgba(" + parseInt(result[1], 16) + ", " + parseInt(result[2], 16) + ", " + parseInt(result[3], 16) + ", " + opacity + ")" : hex;
 }
 
 function notnull(x) {
